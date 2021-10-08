13e7a0013727baf7e92d4bce931351cdd2e24e77
Leo Ling <leocabbage2008@gmail.com>
2021-10-08 19:51:35 -0400

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Layout from './components/Layout';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
