87a1d3587d9e6ec880824280e3052059e5c96fb3
Leo Ling <leocabbage2008@gmail.com>
2021-10-10 23:50:50 -0400

    totop

--- src/components/ScrollToTop/index.jsx
+++ src/components/ScrollToTop/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './index.css';
 
 export default function ScrollToTop() {
   const classNames = (...classes) => {
@@ -20,24 +21,23 @@ export default function ScrollToTop() {
       behavior: 'smooth',
     });
   };
-
   useEffect(() => {
     window.addEventListener('scroll', toggleVisibility);
-
     return () => {
       window.removeEventListener('scroll', toggleVisibility);
     };
   }, []);
 
   return (
-    <div className='fixed bottom-2 right-2'>
+    <div className='fixed bottom-0 right-0 mb-3 mr-3'>
       <button
         type='button'
         onClick={scrollToTop}
         className={classNames(
           isVisible ? 'opacity-100' : 'opacity-0',
-          'inline-flex items-center p-3 rounded-full shadow-sm text-white bg-pink-600 transition-opacity hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500'
+          'duration-200 inline-flex items-center p-3 rounded-full bg-gray-400 transition-opacity hover:bg-gray-500 hover:shadow-xl'
         )}
+        id='totop'
       >
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -53,7 +53,6 @@ export default function ScrollToTop() {
             d='M5 10l7-7m0 0l7 7m-7-7v18'
           />
         </svg>
-        ;
       </button>
     </div>
   );
