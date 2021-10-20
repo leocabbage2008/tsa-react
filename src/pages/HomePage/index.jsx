d2d32b82148e2850255c24dce2a9bfafd04fcc7d
Ian <austinyan43@gmail.com>
2021-10-20 19:17:45 -0400

    asdf

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,12 @@
+
 import React from 'react';
 import Layout from '../../components/Layout';
-
+import './index.css';
+ 
 export default function HomePage() {
     return (
         <Layout>
-            <h1 className="text-xl my-2 font-sans">Lantern Festival</h1>
-            <p>joe mama</p>
+            <h1 className="header text-xl my-2 font-sans">Lantern Festival</h1>
         </Layout>
     )
 }
