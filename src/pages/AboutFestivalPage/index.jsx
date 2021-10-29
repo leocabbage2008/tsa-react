6a4b2522a73d6798c6974d36b96e60640151979b
Ian <austinyan43@gmail.com>
2021-10-29 18:03:44 -0400

    ajshdfjagsdkfhgas

--- src/pages/AboutFestivalPage/index.jsx
+++ src/pages/AboutFestivalPage/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import './index.css';
 export default function AboutFestivalPage() {
   return (
     <Layout>
-      <h1>About festival</h1>
+      <h1 className='header text-xl my-2 font-sans'>Lantern Festival </h1>
+      <p>
+      <h2>The Lantern Festival is a festival that's been celebrated for over 2000 years.</h2>
+      <br />
+      Starting in China in 358 BCE, peasants and royalty alike have been
+      sending lanterns skywards for good luck. There are many saying as to how
+      this tradition started, but the most common saying is that the Emperor
+      Mig of the Han dynasty noticed that Buddhist monks would light lanterns
+      in temples on the 15th day of the first lunar month. Because of this, he
+      ordered that all households, temples, and palaces light lanterns on that
+      evening. Eventually, everyone got used to this and it became a folk
+      custom.
+      <br />{' '}
+      The Lantern Festival is a festival that's been celebrated for over 2000 years.
+      </p>
     </Layout>
   );
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,23 +5,10 @@ import './index.css';
 export default function HomePage() {
   return (
     <Layout>
-      <h1 className='header text-xl my-2 font-sans'>Lantern Festival </h1>
-      <p>
-        <br />
-        <br />
-        <br /> The Lantern Festival is a festival that's been celebrated for
-        over 2000 years.
-        <br />
-        Starting in China in 358 BCE, peasants and royalty alike have been
-        sending lanterns skywards for good luck. There are many saying as to how
-        this tradition started, but the most common saying is that the Emperor
-        Mig of the Han dynasty noticed that Buddhist monks would light lanterns
-        in temples on the 15th day of the first lunar month. Because of this, he
-        ordered that all households, temples, and palaces light lanterns on that
-        evening. Eventually, everyone got used to this and it became a folk
-        custom.
-        <br />{' '}
-      </p>
+      <div className="img-div"></div>
+      <div className = "inner-div">
+          <h1 className='header text-xl my-2 font-sans'>Lantern Festival </h1>
+      </div>
     </Layout>
   );
 }
