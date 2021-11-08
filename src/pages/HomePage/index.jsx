2ad199644b5d816f8a03269d3a325ca4508a89fe
Ling <lxl24@ad.scasd.org>
2021-11-08 17:10:06 -0500

    lol

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import './index.css';
+import banner from './banner.jpg';
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1 className='text-xl my-2 relative bg-center overflow-hidden'>Lantern Festival </h1>
+      <div>
+        <div className="flex filter blur-lg m-1 overflow-hidden banner-wrapper">
+          <img src={banner} alt="banner"/>
+        </div>
+        <h1 className='text-3xl text-white header text-center relative bottom-10'>Lantern Festival </h1>
+      </div>
     </Layout>
   );
 }
