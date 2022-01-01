44ddebc68183dc9205df62748c0af6676a19c913
Leo Ling <leocabbage2008@gmail.com>
2022-01-01 15:29:04 -0500

    more shit

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import './index.css';
 import banner from './banner.jpg';
+import { classNames } from '../../utils';
 
 export default function HomePage() {
   return (
     <Layout>
-      <div className='p-2 mt-0.5 relative'>
+      <div className='p-2 mt-0.5 relative h-auto'>
         <img
-          className='filter blur-md object-cover half-height w-full text-center'
+          className='filter blur-md object-cover vh w-full text-center'
           src={banner}
           alt='banner'
         />
-        <p className='typography text-7xl text-white font-semibold centered outline'>
+        <p
+          className={classNames(
+            'text-5xl sm:text-5xl md:text-6xl lg:text-9xl text-white font-semibold centered outline'
+          )}
+        >
           Chinese Lantern Festival
         </p>
       </div>
-      <hr className='opacity-0' />
+      <hr className='opacity-50 mb-3' />
       <div className='content'>
-        <p className='text-center text-xl'>The Lantern Festival</p>
+        <p className='text-center text-xl'>
+          The Lantern Festival is a festival that goes back more than 2,000
+          years ago
+        </p>
       </div>
     </Layout>
   );
