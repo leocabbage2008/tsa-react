651cfd401493db621a131db23599c0a545670e25
Leo Wang <lzw12@scasd.org>
2022-01-19 12:36:16 -0500

    events page part 1
    i did this in school the main changes are @ home

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -1,43 +1,35 @@
-import React from 'react';
-import Layout from '../../components/Layout';
-import './index.css';
-import Banner from '../../components/Banner';
-import banner from './banner.jpg';
-import Grid from '../../components/Grid';
+import React from "react";
+import Layout from "../../components/Layout";
+import "./index.css";
+import Banner from "../../components/Banner";
+import banner from "./banner.jpg";
+import Grid from "../../components/Grid";
+// event images
+import tangyuan from "./Events Images/Tangyuan.jpg";
 
 export default function EventsPage() {
   const cards = [
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
-    { src: 'https://picsum.photos/200/300', title: 'title', text: 'text' },
+    {
+      src: tangyuan,
+      title: "Tangyuan (10:00 - 11:45)",
+      text: "Learn how to make the traditional Tangyuan, following a recipe ",
+    },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
+    { src: "https://picsum.photos/200/300", title: "title", text: "text" },
   ];
   return (
     <Layout>
-      <Banner img={banner} text='Events' />
-      <div className='content mx-4 flex flex-col justify-center'>
-        <div className='body relative -top-1'>
-          <p className='text-center text-3xl'>
-            The Magical Chinese Lantern Festival
-          </p>
-          <p className='text-center mt-6 text-2xl'>
-            From the comfort of your own home!
-          </p>
-          <p className='mx-14 mt-6 text-2xl'>
-            Due to the Covid-19 pandemic, we will not be hosting our annoual
-            celebrations online. Instead, we're having our completely free
-            events online. Click the button above to learn more about our
-            events, or click to learn more about the Chinese Lantern Festival.
-          </p>
-        </div>
+      <Banner img={banner} text="Events" />
+      <div className="content mx-4 flex flex-col justify-center">
         <Grid cards={cards} />
       </div>
     </Layout>
