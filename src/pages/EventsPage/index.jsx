3e8b9a2b37418f2cb6bbf1311e3fa9e53caf3fe1
Ian <austinyan43@gmail.com>
2022-01-20 18:38:41 -0500

    I don't like the yellow dot

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -25,8 +25,15 @@ export default function EventsPage() {
       <Banner img={banner} text='Events' />
       <div className='content mx-4 flex flex-col justify-center'>
         <div className='body relative -top-1'>
-          <p>
-            
+          <p className='text-center text-3xl'>
+            Ways to celebrate Lantern Festival
+          </p>
+          <p className='text-center mt-6 text-2xl'>
+            From the comfort of your own home!
+          </p>
+          <p className='mx-14 mt-6 text-2xl'>
+            Here are 12 events that you can do throughout the day to celebrate this important day in Chineaaaaaaaaaaase culture. We recommend you to do as many of them as possible, and have a great time.
+            Also, if you don't like the image generated on the cards, you can just reload and a new image will appear.
           </p>
         </div>
         <Grid cards={cards} />
