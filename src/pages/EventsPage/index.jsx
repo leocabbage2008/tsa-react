c02b17097d94895ad4d341b7c8c62512cdd5e9ff
Leo Ling <leocabbage2008@gmail.com>
2022-01-21 22:17:18 -0500

    oopsies just part 2 of last thing

--- src/pages/EventsPage/index.jsx
+++ src/pages/EventsPage/index.jsx
@@ -5,11 +5,11 @@ import Banner from '../../components/Banner';
 import banner from './banner.jpg';
 import Grid from '../../components/Grid';
 // event images
-import tangyuan from "./Events Images/Tangyuan.jpg";
-import riddle from "./Events Images/Riddle.jpeg";
-import lantern from "./Events Images/Lantern.jpg";
-import poems from "./Events Images/Poems.jpg";
-import festival from "./Events Images/FestivalShow.jpg";
+import tangyuan from './cardImages/TangYuan.jpg';
+import riddle from './cardImages/Riddle.jpeg';
+import lantern from './cardImages/Lantern.jpg';
+import poems from './cardImages/Poems.jpg';
+import festival from './cardImages/FestivalShow.jpg';
 import Card from '../../components/Card';
 
 export default function EventsPage() {
@@ -34,8 +34,8 @@ export default function EventsPage() {
     },
     {
       src: festival,
-      title: "Lantern Festival Show",
-      time: "7:00PM-10:00PM",
+      title: 'Lantern Festival Show',
+      time: '7:00PM-10:00PM',
       text: "Watch dancers, comedians, singers and actors perform for the festivities, live from China! This marks the end of the holiday season, so it's very energetic.",
     },
   ];
