04e2823af6f9186c49a82a652782ef2c47d2acf6
Leo Wang <lwgames0423@gmail.com>
2022-03-16 17:05:36 -0400

    hgdf

--- src/pages/detailedEvents/index.jsx
+++ src/pages/detailedEvents/index.jsx
@@ -1,1 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import banner from "../../images/bannerAboutUs.webp";
+import Banner from "../../components/Banner";
 
+export default function AboutUsPage() {
+  return <Layout></Layout>;
+}
