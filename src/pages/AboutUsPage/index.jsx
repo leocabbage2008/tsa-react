912357446090de88f3acd0b42ab2ed10e19074de
Leo Ling <leocabbage2008@gmail.com>
2022-04-02 17:08:10 -0400

    swagness

--- src/pages/AboutUsPage/index.jsx
+++ src/pages/AboutUsPage/index.jsx
@@ -11,7 +11,7 @@ export default function AboutUsPage() {
         <p className='text-center text-2xl'>Who Are We?</p>
         <p className='text-center text-xl mt-1'>
           We are Pennsylvania Region 8 TSA's Website Design Team 1023-901, with
-          members 1023-013, 1023-011 and 1023-033. We chose the Lantern Festival
+          members 1023-007, 1023-008 and 1023-006. We chose the Lantern Festival
           as our theme because it aims to promote reconciliation, peace, and
           forgiveness. It's also something that we personally have celebrated
           before and we love how energetic and beautiful the celebrations are.
