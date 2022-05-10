2cc55146abc7fb3bc133b44ae2f908df8e382495
Leo Ling <leocabbage2008@gmail.com>
2022-05-09 22:22:16 -0400

    here ian i did your stuff for you and it looks responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './ThemeDark.css';
-import './ThemeLight.css';
+import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,52 +1,39 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import './index.css';
-// import banner from '../../images/bannerHomePage.webp';
-// import Banner from '../../components/Banner';
 
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Banner img={banner} text='Lantern Festival' /> */}
-      <h1 className=''>Lantern Festival</h1>
-      {/* <a
-          className='button btn mx-auto relative -top-14 sm:px-5 text-lg event-btn'
-          href='/events'
-        >
-          Visit Events Here
-        </a> */}
-      <br />
-      <br />
-      <br />
-      <div className='h2'>
-        <h2 className='text-center font-bold'>
-          Experience the Magical Chinese Lantern Festival From the comfort of
-          your own home
-        </h2>
+      <div className='splash'>
+        <h1 className='banner-header'>Lantern Festival</h1>
+        <div className='banner-sub'>
+          <div className='text-center font-bold'>
+            Experience the Magical Chinese Lantern Festival From the comfort of
+            your own home
+          </div>
+        </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
       <hr />
-      <br />
-      <br />
-      <br />
-      <div className='container_online'>
-        <h3>Online format</h3>
-        <p className='sm:mx-14 mt-6 text-2xl left'>
-          Due to the Covid-19 pandemic, we will not be hosting our annual
-          celebrations in-person. Instead, we're going to be hosting our events
-          online on Febuary 15th, 2022. Click the above button to learn more
-          about our events, or learn more about the Chinese Lantern Festival{' '}
-          <a href='/festival/about' className='link'>
-            here.
-          </a>{' '}
-        </p>
+      <div className='lg:flex lg:flex-column flex-row justify-center'>
+        <div className='content'>
+          <h3>Online format</h3>
+          <p className='sm:mx-14 mt-6 text-2xl'>
+            Due to the Covid-19 pandemic, we will not be hosting our annual
+            celebrations in-person. Instead, we're going to be hosting our
+            events online on February 15th, 2022. Click the above button to
+            learn more about our events, or learn more about the Chinese Lantern
+            Festival{' '}
+            <a href='/festival/about' className='link'>
+              here.
+            </a>
+          </p>
+        </div>
+        <img
+          className='lg:x-0 mx-auto mt-3 px-1 h-full'
+          src='https://picsum.photos/500/300'
+        />
       </div>
-      <br />
-      <br />
-      <br />
       <hr />
       <p className='sm:mx-14 mt-6 text-2xl'>
         During this event, we will be making lanterns, guessing riddles, making
