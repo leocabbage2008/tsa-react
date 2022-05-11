c137806f9e01b8733f227dab9f91f83647cd63d9
Ian <austinyan43@gmail.com>
2022-05-11 18:35:17 -0400

    a

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -14,30 +14,27 @@ export default function HomePage() {
         Experience the Magical Chinese Lantern Festival from the comfort of your
         own home
       </h2>
-      <div className='grid grid-cols-12'>
-        <div className='col-start-2 col-end-12'>
-          <div className='lg:flex lg:flex-column flex-row justify-center'>
-            <div className='content'>
+      <div className='h-screen grid grid-cols-6'>
+        <div className='col-start-2 col-end-6'>
+          <div className='lg:grid grid-cols-5 lg:grid-column grid-row justify-center'>
+            <div className='content col-start-1 col-end-4'>
               <h3>Online format</h3>
-              <div className="online-text">
                 <p className='sm:mx-14 mt-6 text-2xl'>
                   Due to the Covid-19 pandemic, we will not be hosting our annual
                   celebrations in-person. Instead, we're going to be hosting our
-                  events online on February 15th, 2022. Click the above button to
-                  learn more about our events, or learn more about the Chinese
-                  Lantern Festival{' '}
+                  events online on February 15th, 2022. Before then, you can learn about the Lantern Festival{' '}
                   <a href='/festival/about' className='link'>
                     here.
                   </a>
                 </p>
-              </div>
             </div>
             <img
-              className='lg:x-0 mx-auto mt-3 px-1 h-full'
+              className='lg:x-0 mx-auto px-1 h-full col-start-4 col-end-6'
               src={online}
               alt='Join us online'
             />
           </div>
+          <h3>About the event</h3>
           <p className='mt-10 text-2xl'>
             During this event, we will be making lanterns, guessing riddles,
             making traditional Lantern Festival food, and more. This is an
